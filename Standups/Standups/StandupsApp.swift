2d8a59afd4a3ad07801420ad09739d1a4a65aa7b
Akash Soni <akash.soni@tokopedia.com>
2022-11-28 19:30:06 +0530

    day 1 - swiftui

--- Standups/Standups/StandupsApp.swift
+++ Standups/Standups/StandupsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StandupsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StandupsList(model: StandupsListModel())
         }
     }
 }
