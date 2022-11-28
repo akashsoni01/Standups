23c9567d2097365fcac418fb8bbeef7b5b4c0b42
Akash Soni <akash.soni@tokopedia.com>
2022-11-28 18:49:25 +0530

    init

---
+++ Standups/Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Akash Soni on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+        }
+    }
+}
